a773c69a65a8597cb4b2cca64b32cd45f1721439
JoshuaMuwa <muwangujosh@gmail.com>
2022-07-06 08:05:44 -0700

    1--tq

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,43 @@
-#include "holberton.h"
+nclude "main.h"                                                                                                                                                                             
 
-/**
- *
- * print_alphabet - Prints alphabets 'a - z'
- */
+                                                                                                                                                                                             
 
-void print_alphabet(void)
-{
-	char c;
+/**                                                                                                                                                                                          
 
-	for (c = 'a'; c <= 'z'; c++)
-	{
-		_putchar(c);
-	}
-	_putchar('\n');
+ * main - Print "Main" followed by a new line.                                                                                                                                               
 
-}
+ * Description: You are not allowed to include standard libraries.                                                                                                                           
 
+ * Return: 0                                                                                                                                                                                 
+
+ */                                                                                                                                                                                          
+
+int main(void)                                                                                                                                                                               
+
+{                                                                                                                                                                                            
+
+        char main[] = "Main";                                                                                                                                                                
+
+                                                                                                                                                                                             
+
+        int i = 0;                                                                                                                                                                           
+
+                                                                                                                                                                                             
+
+        while (main[i] != '\0')                                                                                                                                                              
+
+        {                                                                                                                                                                                    
+
+                _putchar(main[i]);                                                                                                                                                           
+
+                i++;                                                                                                                                                                         
+
+        }                                                                                                                                                                                    
+
+        _putchar('\n');                                                                                                                                                                      
+
+                                                                                                                                                                                             
+
+        return (0);                                                                                                                                                                          
+
+} 
