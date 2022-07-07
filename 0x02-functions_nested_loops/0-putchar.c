d00fd59b5eb81aa692d8f7d3fb41209cf93d4e0b
JoshuaMuwa <muwangujosh@gmail.com>
2022-07-07 06:28:53 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,43 +1,13 @@
-nclude "main.h"                                                                                                                                                                             
+#include <stdio.h>
 
-                                                                                                                                                                                             
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
 
-/**                                                                                                                                                                                          
+{
+printf("_putchar\n");
 
- * main - Print "Main" followed by a new line.                                                                                                                                               
-
- * Description: You are not allowed to include standard libraries.                                                                                                                           
-
- * Return: 0                                                                                                                                                                                 
-
- */                                                                                                                                                                                          
-
-int main(void)                                                                                                                                                                               
-
-{                                                                                                                                                                                            
-
-        char main[] = "Main";                                                                                                                                                                
-
-                                                                                                                                                                                             
-
-        int i = 0;                                                                                                                                                                           
-
-                                                                                                                                                                                             
-
-        while (main[i] != '\0')                                                                                                                                                              
-
-        {                                                                                                                                                                                    
-
-                _putchar(main[i]);                                                                                                                                                           
-
-                i++;                                                                                                                                                                         
-
-        }                                                                                                                                                                                    
-
-        _putchar('\n');                                                                                                                                                                      
-
-                                                                                                                                                                                             
-
-        return (0);                                                                                                                                                                          
-
-} 
+return (0);
+}
---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+nclude <stdio.h>
+#include "main.h"
+/**
+ *_putchar - prints out to the output
+ *Return : Always 0
+ */
+int _putchar(char c)
+{
+        putchar(c);
+
+        return (0);
+ }
